85ff575e0ad689c6d978273542ea7018d9de4190
fathima <fathimanafeesavy@gmail.com>
2020-03-28 11:32:03 +0530

    [fathima]UC_1 validating the first name.

--- regex/userRegistration.sh
+++ regex/userRegistration.sh
@@ -1,1 +1,12 @@
 #!/bin/bash -x
+shopt -s extglob
+echo "Enter your First name"
+read firstname
+namepattern="^[A-Z][a-z]{2,}$"
+if [[ firstname =~ $namepattern ]]
+then
+	echo "Valid"
+else
+	echo "Invalid"
+fi
+
